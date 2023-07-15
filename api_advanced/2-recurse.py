b38bb147f5c3fe18de16821439b90ed328acfa37
tuyishimejohnson <j.tuyishime4@alustudent.com>
2023-07-15 14:33:13 +0200

    A recursive function that queries the Reddit API

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
 """
-Append all the hot post's titles from a particular
-subreddit on a list calleg hot_list.
+2-main
 """
 
 import json
 import requests
+import sys
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-
-    # After is set to none at first.
-    params = {'after': after} if after else {}
-
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-
-        for post in data['data']['children']:
-            title = post['data']['title']
-            hot_list.append(title)
-
-        # set after to the last post of the previous result.
-        after = data['data']['after']
-
-        # call the function with the updated after i.e recursion
-        # to retrieve addition pages.
-        if after:
-            recurse(subreddit, hot_list, after)
-
-    else:
+    """get all hot post titles for a subreddit recursively"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {}
+    if after:
+        params['after'] = after
+    result = requests.get(url, headers=headers,
+                          params=params, allow_redirects=False)
+    if result.status_code != 200:
         return None
-
-    return hot_list
+    body = result.json()
+    children = body['data']['children']
+    for child in children:
+        hot_list.append(child['data']['title'])
+    if not body['data']['after']:
+        return hot_list
+    else:
+        return recurse(subreddit, hot_list, after=body['data']['after'])
